074f2be9474b9b12c886886945ee75be47967cbc
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-01-18 18:16:35 +0530

    :zap: Created Rule 1

--- lib/commands/rules.py
+++ lib/commands/rules.py
@@ -1,9 +1,17 @@
 from discord.ext.commands import Cog
 from discord.ext.commands import command
+from discord import Embed
+from datetime import datetime
 
 class Rules(Cog):
     def __init__(self,bot):
         self.bot = bot
+
+    @command(name="rule1",aliases=["r1", "rle1"], hidden=True)
+    async def rule1(self,ctx):
+        embed = Embed(title="Rule 01", description="Stay Respectful!!", colour=0x00FFFF, timestamp=datetime.utcnow())
+        embed.set_author(name="Otakustan")
+        await ctx.send(embed=embed)
 
     @Cog.listener()
     async def on_ready(self):
