a55c85e913b19828020f42e26a9ab4900d14ac35
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-01-26 17:09:07 +0530

    :zap: follow dare insta command

--- lib/commands/insta.py
+++ lib/commands/insta.py
@@ -7,6 +7,11 @@ class Insta(Cog):
     def __init__(self,bot):
         self.bot = bot
 
+    @command(name="instadarechan", aliases=["idare", "instadare"])
+    async def instaDare(self,ctx):
+        embed = Embed(title="Follow Dare Chan on Instagram", description="https://www.instagram.com/darechandesu/?hl=en", colour=0x00FFFF, timestamp=datetime.utcnow())
+        await ctx.send(embed=embed)
+
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
